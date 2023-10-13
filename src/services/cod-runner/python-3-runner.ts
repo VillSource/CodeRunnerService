c9b79c90e7fd2c1dfec73795a757df9a00aac19f
VillSource <anirut1800@gmail.com>
2023-10-13 22:46:14 +0700

    feature: 🐍 add python runner support

---
+++ src/services/cod-runner/python-3-runner.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { CodeDto } from "../../common/dto/code-dto";
+import { FilesStorageStrategyFactory } from "../files-storage/files-storage-stratregy.factory";
+import { ICodeRunner } from "./interfaces/cod-runner.interface";
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { environment } from "../../configs/environment";
+import { BaseRunner } from "./base-runner";
+
+
+export class Python3Runner extends BaseRunner implements ICodeRunner {
+
+  private _slave_host = `${environment.SLAVE_USER_NAME}@${environment.SLAVE_HOST_PYTHON}`
+
+  protected createChildProcess(code: CodeDto): ChildProcessWithoutNullStreams {
+    const name = this.fileStorage.saveSourceCode(code.sourcecode, "py")
+    return spawn("ssh", `${this._slave_host} python /config/sourcecode/${name}`.split(' ') );
+  }
+
+}
